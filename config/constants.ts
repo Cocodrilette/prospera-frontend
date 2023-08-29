71f164310f94b8407bf0d952debd2d484228805d
cocodrilette <cocodrilette@gmail.com>
2023-08-28 21:51:25 -0500

    Feat: Config file made to centralized global app variables

---
+++ config/constants.ts
@@ -0,0 +1,13 @@
+export const constants = {
+  providers: {
+    paypal: {
+      authUrl: process.env.NEXT_PUBLIC_PAYPAL_AUTH_URL,
+      clientId: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_ID,
+      clientSecret: process.env.NEXT_PUBLIC_PAYPAL_CLIENT_SECRET,
+    },
+    walletConnect: {
+      appName: process.env.NEXT_PUBLIC_WALLETCONNECT_APP_NAME,
+      projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID,
+    },
+  },
+}
