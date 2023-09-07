c382c88499f2ec31cd923057379c7d54eb872921
cocodrilette <cocodrilette@gmail.com>
2023-09-06 23:41:12 -0500

    Fix: Hydratation error fixed

--- pages/payment/index.tsx
+++ pages/payment/index.tsx
@@ -4,13 +4,23 @@ import { PayPalProvider } from "../../components/providers/PaypalProvider"
 import { Layout } from "../../components/layout/layout"
 import { useAccount } from "wagmi"
 import { ConnectYourWallet } from "../../components/alerts/connectYouWallet"
+import { useEffect, useState } from "react"
 
 const Payment: NextPage = () => {
-  const userAccount = useAccount()
+  const { address, isConnecting, isConnected } = useAccount()
+  const [domLoaded, setDomLoaded] = useState(false)
+
+  useEffect(() => {
+    setDomLoaded(true)
+  })
 
   return (
     <Layout headerOptions={{ title: "Payment", description: "Payment" }}>
-      {userAccount.address ? <PayPalProvider /> : <ConnectYourWallet />}
+      {!domLoaded ? (
+        <div>Loading...</div>
+      ) : (
+        <>{!isConnected ? <ConnectYourWallet /> : <PayPalProvider />}</>
+      )}
     </Layout>
   )
 }
