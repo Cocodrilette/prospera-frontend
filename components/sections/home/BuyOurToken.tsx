408519da669b408733f32d4d5957c590937d0e76
cocodrilette <cocodrilette@gmail.com>
2023-09-17 02:02:38 -0500

    Fix: Special characters scaped required by the compiler

--- components/sections/home/BuyOurToken.tsx
+++ components/sections/home/BuyOurToken.tsx
@@ -6,11 +6,11 @@ export function BuyOurToken() {
     <ScreenSection className="bg-black" id="buy-cielo">
       <div className="flex flex-col items-center justify-center max-w-7xl p-5 m-auto h-full">
         <h2 className="text-5xl md:text-6xl lg:text-7xl font-bold text-gray-300 text-center mb-10">
-          Buy your <span className="text-indigo-600">Cielo's</span>
+          Buy your <span className="text-indigo-600">Cielo&lsquo;s</span>
         </h2>
         <div className="flex flex-col gap-5">
           <p className="text-xl text-gray-400 max-w-3xl ">
-            Cielo is the Prospera's native{" "}
+            Cielo is the Prospera&lsquo;s native{" "}
             <ExternalLink href="https://ethereum.org/en/developers/docs/standards/tokens/erc-20/">
               ERC-20 token
             </ExternalLink>{" "}
@@ -18,8 +18,9 @@ export function BuyOurToken() {
             <ExternalLink href="https://polygon.technology/home">
               Polygon ZK
             </ExternalLink>
-            , blending Ethereum's security with Polygon's speed. It's your
-            gateway to our protocol, serving for fees, contracts, and liquidity.
+            , blending Ethereum&lsquo;s security with Polygon&lsquo;s speed.
+            It&lsquo;s your gateway to our protocol, serving for fees,
+            contracts, and liquidity.
           </p>
         </div>
         <div className="flex flex-col items-center justify-center gap-10">
