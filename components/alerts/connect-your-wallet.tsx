ace2501ba021a45f08271b5c1639feb3128652ab
cocodrilette <cocodrilette@gmail.com>
2023-10-29 00:40:53 -0500

    Feat: ConnectWallet alert redesigned

--- components/alerts/connect-your-wallet.tsx
+++ components/alerts/connect-your-wallet.tsx
@@ -1,10 +1,11 @@
+import { ConnectButton } from "@rainbow-me/rainbowkit"
 import { StrongText } from "../common/strong-text"
 
 export function ConnectYourWallet() {
   return (
-    <div className="flex flex-col m-5 md:m-auto py-20 px-10 shadow-xl rounded-xl bg-yellow-50 md:max-w-md lg:max-w-lg gap-5 md:gap-10">
+    <div className="flex flex-col m-2 md:m-auto py-20 px-10 shadow-xl rounded-xl bg-gray-950 text-white md:max-w-md lg:max-w-lg gap-5 md:gap-10">
       <StrongText>Connect your wallet to continue</StrongText>
-      <p className="text-gray-500 md:text-3xl">
+      <p className="text-gray-400 md:text-3xl">
         If you don&lsquo;t have a wallet, you can create one{" "}
         <a
           className="text-indigo-600"
@@ -16,6 +17,7 @@ export function ConnectYourWallet() {
         </a>
         .
       </p>
+      <ConnectButton />
     </div>
   )
 }
