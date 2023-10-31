cb9c7da579f9bc1538cf41bbec64265843c6eadd
cocodrilette <cocodrilette@gmail.com>
2023-10-30 21:24:45 -0500

    Feat: Component update to acept a child component to optionally extends it

--- components/alerts/connect-your-wallet.tsx
+++ components/alerts/connect-your-wallet.tsx
@@ -1,9 +1,17 @@
 import { ConnectButton } from "@rainbow-me/rainbowkit"
 import { StrongText } from "../common/strong-text"
 
-export function ConnectYourWallet() {
+interface ConnectYourWalletProps {
+  children?: React.ReactNode
+}
+
+export function ConnectYourWallet(
+  { children }: ConnectYourWalletProps = {
+    children: null,
+  }
+) {
   return (
-    <div className="flex flex-col m-2 md:m-auto py-20 px-10 shadow-xl rounded-xl bg-gray-950 text-white md:max-w-md lg:max-w-lg gap-5 md:gap-10">
+    <div className="flex flex-col m-2 md:m-auto py-10 px-5 md:py-20 md:px-10 shadow-xl rounded-xl bg-gray-950 text-white md:max-w-md lg:max-w-lg gap-5 md:gap-10">
       <StrongText>Connect your wallet to continue</StrongText>
       <p className="text-gray-500 md:text-2xl my-2">
         If you don&lsquo;t have a wallet, you can create one{" "}
@@ -17,6 +25,7 @@ export function ConnectYourWallet() {
         </a>
         . If you already have one, make sure your wallet is connected
       </p>
+      {children}
       <ConnectButton />
     </div>
   )
