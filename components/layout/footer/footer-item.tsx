a419b34353d90887cc0dc9aa4468a77e8748abea
cocodrilette <cocodrilette@gmail.com>
2023-11-05 22:54:46 -0500

    Fix key prop in FooterItem component

--- components/layout/footer/footer-item.tsx
+++ components/layout/footer/footer-item.tsx
@@ -6,23 +6,27 @@ interface Item {
 interface FooterItemProps {
   title: string
   items: Item[]
-  id: string
+  key: string
 }
 
-export function FooterItem({ title, items, id }: FooterItemProps) {
+export function FooterItem({ title, items, key }: FooterItemProps) {
   return (
-    <div id={id}>
+    <div id={key} key={key}>
       <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
         {title}
       </h2>
       <ul className="text-gray-500 dark:text-gray-400 font-medium">
-        {items.map((item, idx) => (
-          <li className="mb-4" id={`footer-li-${idx}`}>
-            <a href={item.href} className="hover:underline">
-              {item.label}
-            </a>
-          </li>
-        ))}
+        {items.map((item, idx) => {
+          const key = `footer-li-${idx}`
+
+          return (
+            <li className="mb-4" id={key} key={key}>
+              <a href={item.href} className="hover:underline">
+                {item.label}
+              </a>
+            </li>
+          )
+        })}
       </ul>
     </div>
   )
