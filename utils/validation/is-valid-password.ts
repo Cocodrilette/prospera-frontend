9ba080cef5e3411c748fdc6ce92b91fb537b2076
cocodrilette <cocodrilette@gmail.com>
2023-11-05 21:25:02 -0500

    Add form validation and loading state to login page

---
+++ utils/validation/is-valid-password.ts
@@ -0,0 +1,5 @@
+export function isValidPassword(password: string): boolean {
+  const regex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+  return regex.test(password)
+}
